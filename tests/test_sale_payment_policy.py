9a32ae312c3b9a72589be5cf045a2754b6ff685d
resteve <resteve@zikzakmedia.com>
2013-04-02 16:51:08 +0200

    FIX typo header test file

--- tests/test_sale_payment_policy.py
+++ tests/test_sale_payment_policy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#This file is part of sale_salesman module for Tryton.
+#This file is part of sale_payment_policy module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 
