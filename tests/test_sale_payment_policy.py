1cd53cd4c3c92a420aede6988684ce1c5995ad22
resteve <resteve@zikzakmedia.com>
2015-12-01 11:20:48 +0100

    Tests

--- tests/test_sale_payment_policy.py
+++ tests/test_sale_payment_policy.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part of sale_payment_policy module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the sale_payment_policy module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SalePaymentPolicyTestCase(unittest.TestCase):
+class SalePaymentPolicyTestCase(ModuleTestCase):
     'Test Sale Payment Policy module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_payment_policy')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_payment_policy')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_payment_policy'
 
 
 def suite():
